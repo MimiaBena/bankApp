d2cdb83f5d5aae071bcee0352439eb5f4111599f
Mokhtaria Benaouali <benaoualimokhtaria@hotmail.com>
2022-08-14 15:28:21 +0200

    Create files of bank app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styles from "./style";
+
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={ `${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={ '${styles.boxWith}' }>
+            NavBar
+          </div>
+      </div>
+
+      <div className={ `bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+    </div>
+  );
+
+export default App;
