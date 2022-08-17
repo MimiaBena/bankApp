a219c6727dfb6e38204754dd586a2187680477c1
Mokhtaria Benaouali <benaoualimokhtaria@hotmail.com>
2022-08-17 18:37:51 +0200

    update  content file

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -30,10 +30,8 @@ const Business = () =>  (
         building credit, earning rewards and saving money. But with hundreds
         of credit cards on the market.
       </p>
-
       <Button styles={`mt-10`} />
     </div>
-
     <div className={`${layout.sectionImg} flex-col`}>
       {features.map((feature, index) => (
         <FeatureCard key={feature.id} {...feature} index={index} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,10 @@ import { navLinks } from '../constants';
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
     const [active, setactive] = useState(false);
-
-      
+  
     return (
         <nav className="w-full flex py-6 justify-between items-center navbar">
             <img src={logo} alt='hoobank' className='w-[124PX] h-[32px]' />
-
             <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
                 {navLinks.map((nav, index) => (
                     <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${
